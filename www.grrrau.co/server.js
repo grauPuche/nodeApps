878c1b2f6c8084dca3d39ed8b19343968a0f2b54
grauPuche <talk@graupuche.info>
2017-11-29 12:37:55 -0500

    servers updated

--- www.grrrau.co/server.js
+++ www.grrrau.co/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 7002;
 
 app.use(express.static(__dirname + '/public'));
 
