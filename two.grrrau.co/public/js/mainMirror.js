d1b5258cd406f627b25ef9e8102536b24dd014c9
grauPuche <talk@graupuche.info>
2017-11-30 19:39:20 -0500

    PubNub

--- two.grrrau.co/public/js/mainMirror.js
+++ two.grrrau.co/public/js/mainMirror.js
@@ -1,36 +1,16 @@
 'use strict';
 
-// Put variables in global scope to make them available to the browser console.
 var video = document.getElementById('video');
 var socket = io.connect();
 
-// var img = $('<img>',{id:n});
-
-var button = document.getElementById('localVideo');
-
-socket.on('connect', function() {
+socket.on('connect', function () {
   console.log("Connected");
 });
 
 socket.on('image', function (data) {
-console.log("Got image");
-$('<img>').attr('src', data).appendTo('#mosaic');
+  console.log("Got image");
+  $('<img>').attr('src', data).appendTo('#mosaic');
 });
-
-// button.onclick = function () {
-//   console.log('local click')
-//   var clr = document.getElementById('picker').value;
-//   console.log(clr)
-//   canvas.getContext('2d').globalAlpha=1;
-//   canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)  
-//   canvas.getContext('2d').globalAlpha=0.2;
-//   canvas.getContext('2d').fillStyle = '#' + clr;
-//   canvas.getContext('2d').fillRect(0, 0, canvas.width, canvas.height)
-//   $('body').css('background-color', '#' + clr )
-//   var dataUrl = canvas.toDataURL('image/webp', 1);
-//   document.getElementById('localPhoto').src = dataUrl;
-//   socket.emit('image', dataUrl);
-// }
 
 var constraints = {
   audio: false,
@@ -51,8 +31,10 @@ then(handleSuccess).catch(handleError);
 
 var socket = io();
 
-socket.on('new guess', function(guess){
-	console.log(guess);	
-  $('.givenGuess').css('display','inline')
+socket.on('new guess', function (guess) {
+  console.log(guess);
+  $('.givenGuess').css('display', 'inline')
   $('.givenGuess').html(guess)
 });
+
+var video_out = document.getElementById("vid-box");
