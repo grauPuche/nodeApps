8a69e6b1584a1f492fc6643167c287c69481abe5
grauPuche <talk@graupuche.info>
2017-12-10 10:29:23 -0500

    ajax

--- two.grrrau.co/public/js/mainSender.js
+++ two.grrrau.co/public/js/mainSender.js
@@ -5,25 +5,31 @@ var word;
 
 var socket = io();
 
-$.getJSON("assets/wordList.json",function(data){
-	console.log(data);
-});
 
+
+// $.getJSON("assets/wordList.json",function(word){
+// 	console.log(word);
+// });
 $.ajax({
 	url: 'assets/wordList.json',
 	dataType: 'json',
 	type: 'get',
-	cache: ''
+	cache: false,
+	success: function API(word){
+		// $(word.list).each(function(index,value) {
+		// 	console.log(value.word)
+		// })
+		console.log(word.list[0].word);
+	}
 
 })
-
-alert(word.list[0].word)
-
 function search(ele) {
 	if (event.keyCode == 13) {
 
 		var guess = ele.value
 		socket.emit('new guess', guess);
+
+		console.log(word.list[0].word)
 
 		// alert(ele.value);
 		$('.bigMsg').attr('class','smallMsg');
