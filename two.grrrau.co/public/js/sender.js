d98bbeb9ded363974f1cded551ad1db7dca2575c
grauPuche <talk@graupuche.info>
2017-12-14 15:28:37 -0500

    all getting puns

--- two.grrrau.co/public/js/sender.js
+++ two.grrrau.co/public/js/sender.js
@@ -1,28 +1,28 @@
-
 var connected = false;
 var typing = false;
 var words;
-
+var response;
+var n = 0;
 var socket = io();
 
 
 
-// $.getJSON("assets/wordList.json",function(word){
-// 	console.log(word);
-// });
-// $.ajax({
-// 	url: 'assets/wordList.json',
-// 	dataType: 'json',
-// 	type: 'get',
-// 	cache: false,
-// 	success: function API(word){
-// 		// $(word.list).each(function(index,value) {
-// 		// 	console.log(value.word)
-// 		// })
-// 		console.log(word.list[0].word);
-// 		words = word
-// 	}
-// })
+$.getJSON("assets/wordList.json",function(word){
+	console.log(word);
+});
+$.ajax({
+	url: 'assets/wordList.json',
+	dataType: 'json',
+	type: 'get',
+	cache: false,
+	success: function API(word){
+		// $(word.list).each(function(index,value) {
+		// 	console.log(value.word)
+		// })
+		console.log(word.list[0].word);
+		words = word
+	}
+})
 function search(ele) {
 	if (event.keyCode == 13) {
 
@@ -32,23 +32,27 @@ function search(ele) {
 		// console.log(words.list[0].word)
 
 		// alert(ele.value);
-		$('.bigMsg').attr('class','smallMsg');
-		$('.bigBox').attr('class','smallBox');
+		$('.bigMsg').attr('class', 'smallMsg');
+		$('.bigBox').attr('class', 'smallBox');
 
 		$('.smallBox').first().remove();
-		
-		$('.sentMsgs').append("<div class='bigBox'><div class='bigMsg'>"+ele.value+"</div></div>");
-		$('.sentMsgs').attr('style','')
+
+		$('.sentMsgs').append("<div class='bigBox'><div class='bigMsg'>" + ele.value + "</div></div>");
+		$('.sentMsgs').attr('style', '')
 		ele.value = "";
+		$('html,body').animate({
+				scrollTop: $("#empty").offset().top
+			},
+			'slow');
 	}
 }
 
-socket.on('new guess', function(guess){
-	console.log(guess);	
+socket.on('new guess', function (guess) {
+	console.log(guess);
 });
 
-socket.on('isItRight',function(isItRight){
-	if(isItRight == true){
+socket.on('isItRight', function (isItRight) {
+	if (isItRight == true) {
 		console.log('YES!! RIGHT ANSWER!!')
 		$('.bigMsg').addClass('right');
 		// $('.bigMsg').removeClass('#right');
@@ -56,4 +60,13 @@ socket.on('isItRight',function(isItRight){
 		console.log('NO!! YOU FUCKED UP!!')
 		$('.bigMsg').addClass('wrong');
 	}
+})
+
+socket.on('response',function(response){
+	$('.sentMsgs').append("<div class='smallBox'><div class='receivedBox'><div class='received'><img src='"+words.list[response].gifs+"' alt=''></div></div></div><div class='smallBox'><div class='receivedBox'><div class='receivedTxt'>"+words.list[response].pun+"</div></div></div>");
+	// $(body).scrollTop($(body).height());	
+	//window.scrollTo(0,document.body.scrollHeight + 100);
+	$('html,body').animate({
+        scrollTop: $("#empty").offset().top},
+        'slow');
 })
