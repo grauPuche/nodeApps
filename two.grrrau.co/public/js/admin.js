ce68e4f56b43a9f047ce6e397e61a24b32aceab8
grauPuche <talk@graupuche.info>
2017-12-15 15:01:06 -0500

    random changes

--- two.grrrau.co/public/js/admin.js
+++ two.grrrau.co/public/js/admin.js
@@ -1,7 +1,55 @@
 var socket = io();
 
-function slider(){
+var n = 0;
+var NEXT = 0;
+var words;
+var nextNEXT = 0;
+var newWordNum = 0;
+
+function slider() {
 	var wordNum = document.getElementById("wordSlider").value;
 	socket.emit('n', wordNum);
 	console.log(wordNum);
+	$('#current').html(words.list[wordNum].word)
 };
+$.ajax({
+	url: 'assets/wordList.json',
+	dataType: 'json',
+	type: 'get',
+	cache: false,
+	success: function API(word) {
+		// $(word.list).each(function(index,value) {
+		// 	console.log(value.word)
+		// })
+		console.log(word.list[0].word);
+		words = word
+	}
+})
+
+socket.on('response', function (response) {
+	NEXT = (response + 1)
+	console.log('the current word is ~ ' + NEXT)
+	$('#current').html(words.list[NEXT].word)
+
+})
+
+function tooHard() {
+	nextNEXT = (NEXT + 1)
+  socket.emit('n', nextNEXT)
+  console.log('the current word value ~ ' + nextNEXT);  
+  console.log('the NEXT value is ~ ' + nextNEXT);
+  $('#current').html(words.list[nextNEXT].word)
+};
+
+function goodEnough(){
+	socket.emit('response', NEXT)
+	nextNEXT = (NEXT + 1)
+	socket.emit('n', nextNEXT)
+	var isItRight = true;
+	socket.emit('isItRight', isItRight);
+}
+
+socket.on('n', function(newWordNum){
+	NEXT = newWordNum;
+	$('#current').html(words.list[newWordNum].word)
+})
