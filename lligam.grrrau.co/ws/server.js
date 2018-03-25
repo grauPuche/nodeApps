65982a483cbd703b288da8ee25588519afaaba92
grauPuche <talk@graupuche.info>
2018-03-25 11:13:06 -0400

    updates lligam

--- lligam.grrrau.co/ws/server.js
+++ lligam.grrrau.co/ws/server.js
@@ -1,9 +1,12 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var path = require('path');
+var public = __dirname + "/public/";
 
-app.get('/', function (req, res) {
-   res.sendFile(__dirname + '/index.html');
-})
-app.listen(3000, function () {
-   console.log('Example app listening on port 3000!')
-})
+app.get('/', function(req, res) {
+    res.sendFile(path.join(public + "index.html"));
+});
+
+app.use('/', express.static(public));
+
+app.listen(3000);
