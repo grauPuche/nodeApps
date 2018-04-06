ab266210fba629155766ba41542db4e217e40f10
grauPuche <talk@graupuche.info>
2018-04-05 21:22:02 -0400

    [ws] change - index.html

--- lligam.grrrau.co/ws/serverLligam_ws.js
+++ lligam.grrrau.co/ws/serverLligam_ws.js
@@ -3,10 +3,10 @@ var app = express();
 var path = require('path');
 var public = __dirname + "/public/";
 
-app.get('/', function(req, res) {
+app.get('/joy', function(req, res) {
     res.sendFile(path.join(public + "index.html"));
 });
 
-app.use('/', express.static(public));
+app.use('/joy', express.static(public));
 
-app.listen(7003);
+app.listen(7003);“
