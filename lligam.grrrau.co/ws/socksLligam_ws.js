2d48e692442d94fa4ad154165af8edca07ee334f
grauPuche <talk@graupuche.info>
2018-04-05 22:16:59 -0400

    [ws] update - split incoming data

--- lligam.grrrau.co/ws/socksLligam_ws.js
+++ lligam.grrrau.co/ws/socksLligam_ws.js
@@ -1,13 +1,13 @@
-var WebSocketServer = require('websocket').server;
-var http = require('http');
-var colors = require('colors/safe');
+var WebSocketServer = require("websocket").server;
+var http = require("http");
+var colors = require("colors/safe");
 
-var server = http.createServer(function (request, response) {
+var server = http.createServer(function(request, response) {
   // process HTTP request. Since we're writing just WebSockets
   // server we don't have to implement anything.
 });
-server.listen(7013, function () {
-  console.log((new Date()) + " Server is listening on port 7013");
+server.listen(7013, function() {
+  console.log(new Date() + " Server is listening on port 7013");
 });
 
 // create the server
@@ -15,31 +15,25 @@ wsServer = new WebSocketServer({
   httpServer: server
 });
 
+var provisionalValueForX = 125;
+var values = [];
+
 // WebSocket server
-wsServer.on('request', function (request) {
+wsServer.on("request", function(request) {
   var connection = request.accept(null, request.origin);
 
-  console.log("client " + colors.green ('connected'))
+  console.log("client " + colors.green("connected"));
 
   // This is the most important callback for us, we'll handle
   // all messages from users here.
-  connection.on('message', function (message) {
-
-    //if (message.type === 'utf8') {
-      // process WebSocket message
-      console.log(' ')
-      // console.log('NEW MESSAGE')
-      // console.log('======')
-      // console.log(Object.keys(message))
-      console.log('new message: ' + colors.yellow(message.utf8Data) )
-      // console.log('======')
-      console.log(' ')
-    //}
-
+  connection.on("message", function(message) {
+    console.log("new message: " + colors.yellow(message.utf8Data));
+    values = message.utf8Data.split(',')
+    console.log(colors.blue("    X" + provisionalValueForX) + colors.green(" Y" + values[0]) + colors.red(" K" + values[1]));
   });
 
-  connection.on('close', function (connection) {
+  connection.on("close", function(connection) {
     // close user connection
-    console.log("client " + colors.red ('disconnected'))
+    console.log("client " + colors.red("disconnected"));
   });
 });
