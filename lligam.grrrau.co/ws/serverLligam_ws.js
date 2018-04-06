1327ea585b70248b25a9cf54616cd1a71976fb5e
grauPuche <talk@graupuche.info>
2018-04-06 14:17:41 -0400

    [ws] add - new require

--- lligam.grrrau.co/ws/serverLligam_ws.js
+++ lligam.grrrau.co/ws/serverLligam_ws.js
@@ -3,6 +3,8 @@ var app = express();
 var path = require('path');
 var public = __dirname + "/public/";
 var port = 7003;
+var colors = require("colors/safe");
+
 
 app.get('/joy', function(req, res) {
     res.sendFile(path.join(public + "index.html"));
