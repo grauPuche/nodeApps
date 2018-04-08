7e6703f5c03dd48f8359fca3697e8757703ba8b4
grauPuche <talk@graupuche.info>
2018-04-08 16:38:10 -0400

    [ws] add - parsing and diplaying json

--- lligam.grrrau.co/ws/public/client.js
+++ lligam.grrrau.co/ws/public/client.js
@@ -1,31 +1,47 @@
-$(function () {
+$(function() {
+
+  var butt = false;
+  var xaxis = 0;
+  var yaxix = 0;
+
   // if user is running mozilla then use it's built-in WebSocket
   window.WebSocket = window.WebSocket || window.MozWebSocket;
 
-  var connection = new WebSocket('ws://mitjons.grrrau.co');
+  var connection = new WebSocket("ws://mitjons.grrrau.co");
 
-  connection.onopen = function () {
-	// connection is opened and ready to use
-  console.log("YAY! connection established")
-  connection.send('~WEB~')
-  $('#serverStatus').html('ONLINE');
+  connection.onopen = function() {
+    // connection is opened and ready to use
+    console.log("YAY! connection established");
+    connection.send("~WEB~");
+    $("#serverStatus").html("offline");
   };
 
-  connection.onerror = function (error) {
-	// an error occurred when sending/receiving data
-  console.log("connection error ma boi")
+  connection.onerror = function(error) {
+    // an error occurred when sending/receiving data
+    console.log("connection error ma boi");
   };
 
-  connection.onmessage = function (message) {
+  connection.onmessage = function(message) {
     // try to decode json (I assume that each message from server is json)
     try {
       var json = JSON.parse(message.data);
-      console.log(message.data)
+      console.log(message.data);
     } catch (e) {
-      console.log('This doesn\'t look like a valid JSON: ',
-          message.data);
+      console.log("This doesn't look like a valid JSON: ", message.data);
       return;
     }
-    // handle incoming message
+
+    if (json.type === "x") {
+      xaxis = json.data;
+      $("#x strong").html(xaxis);
+    } else if (json.type === "y") {
+      yaxis = json.data;
+      $("#y strong").html(yaxis);
+    } else if (json.type === "k") {
+      butt = json.data;
+      $("#k strong").html(butt);
+    } else {
+      console.log("Hmm..., I've never seen JSON like this:", json);
+    }
   };
 });
